8ee9ce1380f9f691100b17a4f34bcfd6587ab25c
Toan Nguyen <ntoand@gmail.com>
2018-03-09 14:05:29 +1100

    Update ClusDoC.m

--- ClusDoC.m
+++ ClusDoC.m
@@ -2,7 +2,7 @@ function ClusDoC(varargin)
     version = '1.0.0';
     fprintf('Clus-DoC version %s\n', version);
     close all; % for easier debugging
-    DEBUG = true;
+    DEBUG = false;
     if(DEBUG)
         addpath('dev'); % "ln -s private dev" to debug line by line in private funcs
     end
